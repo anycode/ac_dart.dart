5f92db5323862d0003acb564f6345e275aa4d1f3
Martin Edlman <ac@an.y-co.de>
2024-01-10 15:40:53 +0100

    Update formatting classes, methods and functions

--- ac_dart/lib/src/extensions/number_ext.dart
+++ ac_dart/lib/src/extensions/number_ext.dart
@@ -1,10 +1,65 @@
-import '../utils/format.dart' as fmt;
+import 'package:ac_dart/ac_dart.dart';
+import 'package:ac_dart/src/format/format_distance.dart' as df;
+import 'package:ac_dart/src/format/format_number.dart' as nf;
+import 'package:ac_dart/src/format/format_percent.dart' as pctf;
+import 'package:ac_dart/src/format/format_price.dart' as pf;
 
 extension NumExt on num {
-  String format([int decimal = 2]) => fmt.formatNumber(this, decimal);
-  String formatCsv() => fmt.formatCsvNumber(this);
-  String formatPrice() => fmt.formatPrice(this);
-  String formatCsvPrice() => fmt.formatCsvPrice(this);
-  String formatDistance({String unit = 'm', double modulo = 1.0, int decimal = 2}) =>
-      fmt.formatDistance(this, unit: unit, modulo: modulo, decimal: decimal);
+  /// Extension method for formatting numbers in human readable format. See [nf.formatNumber] function for details.
+  String format([int decimalDigits = 2]) => nf.formatNumber(this, decimalDigits);
+
+  /// Extension method for formatting numbers in machine readable format. See [nf.formatCsvNumber] function for details.
+  String formatCsv([int decimalDigits = 2]) => nf.formatCsvNumber(this, decimalDigits);
+
+  /// Extension method for formatting numbers for PDF. See [number.formatPdfNumber] function for details.
+  String formatTable([int decimalDigits = 2]) => nf.formatTableNumber(this, decimalDigits);
+
+  /// Extension method for formatting prices in human readable format. See [pf.formatPrice] function for details.
+  String formatPrice([int decimalDigits = 2]) => pf.formatPrice(this, decimalDigits);
+
+  /// Extension method for formatting prices in machine readable format. See [pf.formatCsvPrice] function for details.
+  String formatCsvPrice([int decimalDigits = 2]) => pf.formatCsvPrice(this, decimalDigits);
+
+  /// Extension method for formatting numbers for tables. See [pf.formatTablePrice] function for details.
+  String formatTablePrice([int decimalDigits = 2]) => pf.formatTablePrice(this, decimalDigits);
+
+  /// Extension method for formatting percents in human readable format. See [pctf.formatPercent] function for details.
+  String formatPercent([int decimalDigits = 2]) => pctf.formatPercent(this, decimalDigits);
+
+  /// Extension method for formatting percents in machine readable format. See [pctf.formatCsvPercent] function for details.
+  String formatCsvPercent([int decimalDigits = 2]) => pctf.formatCsvPercent(this, decimalDigits);
+
+  /// Extension method for formatting numbers for tables. See [pctf.formatTablePercent] function for details.
+  String formatTablePercent([int decimalDigits = 2]) => pctf.formatTablePercent(this, decimalDigits);
+
+  /// Shorthand to `FormatDistance({int decimal, FormatDistanceUnit baseUnit}).format(num? number, {FormatDistanceUnit unit})`
+  String formatDistance({int decimalDigits = 2, String unit = 'm'}) => df.formatDistance(this, decimalDigits: decimalDigits, unit: unit);
+
+  /// Shorthand to `FormatDistance({int decimal, FormatDistanceUnit baseUnit}).formatCsv(num? number, {FormatDistanceUnit unit})`
+  String formatCsvDistance({int decimalDigits = 2, double modulo = 1.0}) =>
+      df.formatCsvDistance(this, modulo: modulo, decimalDigits: decimalDigits);
+
+  /// Shorthand to `FormatDistance({int decimal, FormatDistanceUnit baseUnit}).formatPdf(num? number, {FormatDistanceUnit unit})`
+  String formatTableDistance({int decimalDigits = 2, double modulo = 1.0}) =>
+      df.formatTableDistance(this, modulo: modulo, decimalDigits: decimalDigits);
+}
+
+extension DoubleExt on double {
+  /// Shorthand to `FormatLatitude().formatD(num? number, {int decimal})`
+  String formatLatitudeD({int decimalDigits = 6}) => FormatLatitude().formatD(this, decimalDigits: decimalDigits);
+
+  /// Shorthand to `FormatLatitude().formatDM(num? number, {int decimal})`
+  String formatLatitudeDM({int decimalDigits = 4}) => FormatLatitude().formatDM(this, decimalDigits: decimalDigits);
+
+  /// Shorthand to `FormatLatitude().formatDMS(num? number, {int decimal})`
+  String formatLatitudeDMS({int decimalDigits = 3}) => FormatLatitude().formatDMS(this, decimalDigits: decimalDigits);
+
+  /// Shorthand to `FormatLongitude().formatD(num? number, {int decimal})`
+  String formatLongitudeD({int decimalDigits = 6}) => FormatLongitude().formatD(this, decimalDigits: decimalDigits);
+
+  /// Shorthand to `FormatLongitude().formatDM(num? number, {int decimal})`
+  String formatLongitudeDM({int decimalDigits = 4}) => FormatLongitude().formatDM(this, decimalDigits: decimalDigits);
+
+  /// Shorthand to `FormatLongitude().formatDMS(num? number, {int decimal})`
+  String formatLongitudeDMS({int decimalDigits = 3}) => FormatLongitude().formatDMS(this, decimalDigits: decimalDigits);
 }
--- ac_dart/lib/src/utils/format.dart
+++ ac_dart/lib/src/utils/format.dart
@@ -28,62 +28,8 @@
  */
 
 import '../extensions/datetime_ext.dart';
-import 'package:intl/intl.dart';
-
-DateFormat _globalDF = DateFormat('E dd.MM.yyyy', 'cs_CZ');
-DateFormat get globalDateFormat => _globalDF;
-String formatDate(DateTime? date) => date == null ? '' : _globalDF.format(date);
-
-DateFormat _globalSDF = DateFormat('EEEE, dd.MM.', 'cs_CZ');
-DateFormat get globalShortDateFormat => _globalSDF;
-String formatShortDate(DateTime? date) => date == null ? '' : _globalSDF.format(date);
-
-DateFormat _globalBDF = DateFormat('dd.MM.yyyy', 'cs_CZ');
-DateFormat get globalBirthdayFormat => _globalBDF;
-String formatBirthday(DateTime? date) => date == null ? '' : _globalBDF.format(date);
-
-DateFormat _globalTF = DateFormat.Hm();
-DateFormat get globalTimeFormat => _globalTF;
-String formatTime(DateTime? date) => date == null ? '' : _globalTF.format(date);
-
-DateFormat _globalDTF = DateFormat('E dd.MM.yyyy HH:mm', 'cs_CZ');
-DateFormat get globalDateTimeFormat => _globalDTF;
-String formatDateTime(DateTime? date) => date == null ? '' : _globalDTF.format(date);
-
-DateFormat _globalCsvDF = DateFormat('dd.MM.yyyy', 'cs_CZ');
-DateFormat get csvDateFormat => _globalCsvDF;
-String formatCsvDate(DateTime? date) => date == null ? '' : _globalCsvDF.format(date);
-String formatPdfDate(DateTime? date) => date == null ? '' : _globalCsvDF.format(date);
+import '../format/format_datetime.dart';
 
 String formatDateTimeRange(DateTime from, DateTime to) => from.trunc(DateTimeExt.unitDay) == to.trunc(DateTimeExt.unitDay)
     ? '${formatDateTime(from)} - ${formatTime(to)}'
     : '${formatDateTime(from)} - ${formatDateTime(to)}';
-
-NumberFormat _globalCurrFmt = NumberFormat('#0.00', 'cs_CZ');
-String formatPrice(num? price) => price == null ? '' : '${price.toStringAsFixed(2)} Kč';
-String formatCsvPrice(num? price) => price == null ? '' : _globalCurrFmt.format(price);
-String formatPdfPrice(num? price) => price == null ? '' : _globalCurrFmt.format(price);
-
-NumberFormat _globalNumFmt = NumberFormat('#0.##', 'cs_CZ');
-String formatNumber(num? number, [int decimals = 2]) => number == null ? '' : number.toStringAsFixed(decimals);
-String formatCsvNumber(num? number) => number == null ? '' : _globalNumFmt.format(number);
-String formatPdfNumber(num? number) => number == null ? '' : _globalNumFmt.format(number);
-
-String formatDistance(num? distance, {String unit = 'm', double modulo = 1.0, int decimal = 2}) =>
-    distance == null ? '' : '${(distance / modulo).toStringAsFixed(decimal)} $unit';
-
-String formatLatitude(double lat) {
-  return '${_formatLL(lat)}${lat < 0 ? 'S' : 'N'}';
-}
-
-String formatLongitude(double lng) {
-  return '${_formatLL(lng)}${lng < 0 ? 'W' : 'E'}';
-}
-
-String _formatLL(double ll) {
-  final value = ll.abs();
-  final deg = value.floor();
-  final min = ((value - deg) * 60).floor();
-  final sec = (((value - deg) * 60 - min) * 60);
-  return '$deg°$min\'${sec.toStringAsFixed(3)}"';
-}
