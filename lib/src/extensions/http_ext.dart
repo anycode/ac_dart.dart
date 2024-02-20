3ff8f9ed49d5c03c49b4086320e0aaa4f1295367
Martin Edlman <ac@an.y-co.de>
2024-02-20 15:57:10 +0100

    Add new content types to Content Type Extension. Update versions to 0.1.6.

--- lib/src/extensions/http_ext.dart
+++ lib/src/extensions/http_ext.dart
@@ -3,10 +3,19 @@ import 'dart:io';
 extension ContentTypeExt on ContentType {
   /// Content type representing application/error+json mime type
   static final errorJson = ContentType('application', 'error+json', charset: 'utf-8');
+
   /// Content type representing text/csv mime type
   static final csv = ContentType('text', 'csv', charset: 'utf-8');
+
   /// Content type representing application/pdf mime type
   static final pdf = ContentType('application', 'pdf');
+
   /// Content type representing application/zip mime type
   static final zip = ContentType('application', 'zip');
+
+  /// Content type representing multipart/form-data mime type
+  static final formData = ContentType('multipart', 'form-data');
+
+  /// Content type representing application/x-www-form-urlencoded mime type
+  static final formUrlEncoded = ContentType('application', 'x-www-form-urlencoded');
 }
