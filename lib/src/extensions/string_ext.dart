0c5b2b436aa9377175ff4dbab4702a61d53ba8c7
Martin Edlman <ac@anycode.dev>
2025-03-14 08:30:54 +0100

    Move capitalize String extension method from ac_flutter

--- lib/src/extensions/string_ext.dart
+++ lib/src/extensions/string_ext.dart
@@ -1,4 +1,7 @@
 extension StringValidatorExt on String {
+  /// Capitalizes the first letter of the string
+  String get capitalize => '${this[0].toUpperCase()}${substring(1)}';
+
   /// Tests whether the string is valid email address
   bool get isEmail => RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(this);
 }
