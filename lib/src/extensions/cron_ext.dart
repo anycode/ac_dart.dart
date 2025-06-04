e9160a731588b42f83f8bb0067b317fd4fd88939
Martin Edlman <ac@anycode.dev>
2025-06-04 10:32:02 +0200

    Make `DebugLogger` optional in `logSchedule` and remove UUID-based default logger.
    Signed-off-by: Martin Edlman <ac@anycode.dev>

--- lib/src/extensions/cron_ext.dart
+++ lib/src/extensions/cron_ext.dart
@@ -1,35 +1,26 @@
 import 'package:cron/cron.dart';
-import 'package:uuid/uuid.dart';
 
 import '../logger/debug_logger.dart';
 
 extension CronExt on Cron {
-  /// Logger for cron jobs.
-  ///
-  /// It uses [DebugLogger] with a unique prefix for each instance.
-  /// The prefix is the first 8 characters of a UUID v1.
-  static final logger = DebugLogger(
-    name: 'cronjob',
-    printer: DebugPrinter(prefix: Uuid().v1().toString().substring(0, 8)),
-  );
 
   /// Schedules a task and logs the start and end of execution. It's almost the same like [schedule] method,
   /// takes one extra argument to identify the task in logs.
-  void logSchedule(Schedule schedule, Task task, [String taskName = 'unnamed']) {
-    logger.i('Cron task $taskName scheduled to ${schedule.format()}');
+  void logSchedule(Schedule schedule, Task task, {String taskName = 'unnamed', DebugLogger? logger}) {
+    logger?.i('Cron task $taskName scheduled to ${schedule.format()}');
     this.schedule(schedule, () async {
-      logger.i('Cron task $taskName scheduled to ${schedule.format()} started at ${DateTime.now()}');
+      logger?.i('Cron task $taskName scheduled to ${schedule.format()} started at ${DateTime.now()}');
       try {
         final result = await task.call();
         if (result is Future) {
           result.then((value) {
-            logger.i('Cron task $taskName scheduled to ${schedule.format()} finished at ${DateTime.now()} with result $value');
+            logger?.i('Cron task $taskName scheduled to ${schedule.format()} finished at ${DateTime.now()} with result $value');
           });
         } else {
-          logger.i('Cron task $taskName scheduled to ${schedule.format()} finished at ${DateTime.now()} with result $result');
+          logger?.i('Cron task $taskName scheduled to ${schedule.format()} finished at ${DateTime.now()} with result $result');
         }
       } catch(e,st) {
-        logger.f('Cron task $taskName scheduled to ${schedule.format()} failed at ${DateTime.now()}', error: e, stackTrace: st);
+        logger?.f('Cron task $taskName scheduled to ${schedule.format()} failed at ${DateTime.now()}', error: e, stackTrace: st);
       }
     });
   }
